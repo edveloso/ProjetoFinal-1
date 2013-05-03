f2a3593f3bcf044764f415818864172fe8b405a5
leopsantos22 <leopsantos22@yahoo.com.br>
2013-05-03 15:06:55 -0300

    Verificar estas alterações do MB

--- ProjetoFinal/src/br/unigranrio/managedbean/AtorMB.java
+++ ProjetoFinal/src/br/unigranrio/managedbean/AtorMB.java
@@ -1,28 +1,70 @@
 package br.unigranrio.managedbean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.ListDataModel;
 
 import br.unigranrio.bean.requisito.Ator;
+import br.unigranrio.dao.DAO;
 
 @ManagedBean
 @SessionScoped
-public class AtorMB {
+public class AtorMB implements Serializable {
 
+
+	private static final long serialVersionUID = 1L;
+	
 	private Ator ator = new Ator();
 	private ListDataModel<Ator> atores = new ListDataModel<Ator>();
+	private DAO<Ator> dao = new DAO<Ator>(Ator.class);
+	private List<Ator> ato;
+	public Boolean cadastro = true;
 	
-	public String addAtor() {
-		return "addAtores";
+	//Lista de Atores
+	public List<Ator> getAto(){
+		if(ato==null){
+			System.out.println("Carregando Atores.");
+			ato = new DAO<Ator>(Ator.class).getAllOrder("nome");
+		}
+		return ato;
 	}
+	
+	public void addAtor(){
+		for(Ator atores : this.getAtores()){
+			if(atores.getNome().equalsIgnoreCase(this.getAtor().getNome())){
+				this.cadastro = false;
+				break;
+			}
+		}if(this.cadastro == true){
+			if(this.ator.getNome().length() >20){
+				System.out.println("Nome muito grande");
+			}else{
+				if(ator.getId() == null){
+					System.out.println("Cadastro realizado com sucesso");
+					dao.adiciona(ator);
+					this.ator = new Ator();
+				}else{
+					System.out.println("Alteração realizada com sucesso");
+					dao.atualiza(ator);
+				}
+			}
+		}else {
+			System.out.println("Aotr já Registrado");
+		}ato = dao.getAllOrder("Nome");
+		this.cadastro = true;
+			
+		}
+	
+
 	
 	public String salvar(Ator ator){
 		return "listAtores";
 	}
 	
 	public AtorMB() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ListDataModel<Ator> getAtores() {
@@ -40,6 +82,18 @@ public class AtorMB {
 
 	public void setAtor(Ator ator) {
 		this.ator = ator;
+	}
+
+	public DAO<Ator> getDao() {
+		return dao;
+	}
+
+	public void setDao(DAO<Ator> dao) {
+		this.dao = dao;
+	}
+
+	public void setAto(List<Ator> ato) {
+		this.ato = ato;
 	}
 	
 }
