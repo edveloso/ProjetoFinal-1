90f774b94874f04f964de2879e164087b04335df
Marcos Vinícius Pinto <mvpdev@gmail.com>
2013-05-09 00:07:57 -0300

    Acerto na classe para a inicialização dela abrir uma transaction. Isto resolve o problema de várias transactions abertas e o hibernate não conseguir gerenciar.

--- ProjetoFinal/src/br/unigranrio/dao/AbstractHibernateDAO.java
+++ ProjetoFinal/src/br/unigranrio/dao/AbstractHibernateDAO.java
@@ -4,49 +4,46 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public abstract class AbstractHibernateDAO<T extends Serializable> {
 
 	private final Class<T> clazz;
 	private Session session = HibernateUtil.getSession();
+	private Transaction tx; 
 
 	public AbstractHibernateDAO(final Class<T> clazzToSet) {
 		this.session = HibernateUtil.getSession();
+		this.tx = session.beginTransaction();
 		this.clazz = clazzToSet;
 	}
 
 	@SuppressWarnings("unchecked")
 	public T selecionaPorId(final Long id) {
-		session.beginTransaction();
 		return (T) session.get(this.clazz, id);
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<T> retornarTodos() {
-		session.beginTransaction();
 		return session.createQuery("from " + this.clazz.getName()).list();
 	}
 
 	public void gravar(final T entity) {
-		session.beginTransaction();
 		session.save(entity);
 		session.getTransaction().commit();
 	}
 
 	public void atualizar(final T entity) {
-		session.beginTransaction();
 		session.merge(entity);
 		session.getTransaction().commit();
 	}
 
 	public void remover(final T entity) {
-		session.beginTransaction();
 		session.delete(entity);
 		session.getTransaction().commit();
 	}
 
 	public void removerPorId(final Long entityId) {
-		session.beginTransaction();
 		final T entity = this.selecionaPorId(entityId);
 		this.remover(entity);
 		session.getTransaction().commit();
