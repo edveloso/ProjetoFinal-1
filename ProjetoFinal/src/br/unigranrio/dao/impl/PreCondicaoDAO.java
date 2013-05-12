15cf8a5f44798937b4cae6a73c0b143396eb53e8
Marcos Vinícius Pinto <mvpdev@gmail.com>
2013-05-11 23:19:18 -0300

    Pré-condição funcionando filé

--- ProjetoFinal/src/br/unigranrio/dao/impl/PreCondicaoDAO.java
+++ ProjetoFinal/src/br/unigranrio/dao/impl/PreCondicaoDAO.java
@@ -1,15 +1,30 @@
 package br.unigranrio.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import br.unigranrio.bean.requisito.PreCondicao;
 import br.unigranrio.dao.AbstractHibernateDAO;
+import br.unigranrio.dao.HibernateUtil;
 
 @SuppressWarnings("rawtypes")
 public class PreCondicaoDAO extends AbstractHibernateDAO{
+	
+	Session session = HibernateUtil.getSession();
 	
 	@SuppressWarnings("unchecked")
 	public PreCondicaoDAO() {
 		super(PreCondicao.class);
 	}
 
+	@SuppressWarnings("unchecked")
+	public List<PreCondicao> selecionaTodosPorCasoDeUso(long id){
+		Query query = session.createSQLQuery("select * from precondicao where casoDeUso_casoDeUso_id=:id")
+				.addEntity(PreCondicao.class)
+				.setParameter("id", id);
+		return query.list();
+	}
 
 }
