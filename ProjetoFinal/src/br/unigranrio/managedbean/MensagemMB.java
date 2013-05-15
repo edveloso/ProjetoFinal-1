07ad6e4864860d4d031383fed8780f45d1f95eaf
Marcos Vinícius Pinto <mvpdev@gmail.com>
2013-05-14 21:34:48 -0300

    MB para marcar a mensagem no topo da tela com o projeto selecionado

---
+++ ProjetoFinal/src/br/unigranrio/managedbean/MensagemMB.java
@@ -0,0 +1,24 @@
+package br.unigranrio.managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MensagemMB {
+
+	private String mensagem;
+	
+	public MensagemMB() {	
+		mensagem = "Seja bem vindo! Selecione uma opção abaixo.";
+	}
+		
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
